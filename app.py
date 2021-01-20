d6e8e2150d8cc7a63355c8f18676b8d0c6681232
Seong Chan Hwang <34934883+plzprayme@users.noreply.github.com>
2021-01-20 18:21:49 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-
 from flask import Flask
 from sqlalchemy import create_engine, text
 from flask import jsonify
@@ -11,7 +10,6 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 
 database = create_engine(app.config['DB_URL'], encoding='utf8')
-app.database = database
 
 CORS(app)
 
@@ -21,17 +19,23 @@ def get_areas():
     sql = text('SELECT ar.id, name, latitude, longitude, pred_infected, susceptible, infected, site_area, duration, '
                'vent_rate, scale,  DATE_FORMAT(MAX(im.history), "%e, %M, %Y") AS date FROM areas AS ar JOIN images '
                'AS im WHERE ar.id = im.area_id AND im.is_svg = 0 GROUP BY ar.id')
-    rows = app.database.execute(sql)
-    return jsonify(row2json(rows))
+    res = jsonify({})
+    with database.connect() as db:
+        rows = db.execute(sql)
+        res = jsonify(row2json(rows))
+    return res
 
 
 @app.route('/area/<id>/images')
 def get_images(id):
     sql = text('SELECT area_id, DATE_FORMAT(history, "%e, %M, %Y") AS date, image FROM images WHERE '
                'area_id = :id AND is_svg = :bool ORDER BY history DESC')
-    svg_image = app.database.execute(sql.params(id=id, bool=1))
-    area_images = app.database.execute(sql.params(id=id, bool=0))
-    return {'svg': svg_image.fetchone()['image'], 'images': row2json(area_images)}
+    res = jsonify({})
+    with database.connect() as db:
+        svg_image = db.execute(sql.params(id=id, bool=1))
+        area_images = db.execute(sql.params(id=id, bool=0))
+        res = {'svg': svg_image.fetchone()['image'], 'images': row2json(area_images)}
+    return res
 
 
 if __name__ == '__main__':
